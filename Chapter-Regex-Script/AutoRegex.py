501f59e2dd475671745bc7771ecfa235c223804b
Archmage Momo <34354746+ArchmageMomo@users.noreply.github.com>
2018-01-08 12:19:10 +0100

    Small fix to .py and add example.txt
    As long as this syntax ist used, the script works.

--- Chapter-Regex-Script/AutoRegex.py
+++ Chapter-Regex-Script/AutoRegex.py
@@ -6,9 +6,9 @@ def autoregex(path):
         file1=str(file1)
         temp= re.compile(r"\n",re.DOTALL).sub(r"\\\\\n", file1)
         temp = re.sub(r"\*(.*?)\*", r"\\textit{\1}", temp, re.DOTALL)
-        temp = re.sub(r"\\\\\n\\\\\n\\\\\n(.*?)\\\\\n\\\\\n\\\\\n", r"\n\n\n\\newpage\\section{\1}\n\n\n", temp, re.MULTILINE|re.DOTALL)
+        temp = re.sub(r"\\\\\n\\\\\n\\\\\n(.*?)\\\\\n\\\\\n\\\\\n", r"\n\n\n\\newpage\\chapter{\1}\n\n\n", temp, re.MULTILINE|re.DOTALL)
 
-        with open ("%re.txt"%(path),'w') as file2:
+        with open ("%r.tex"%(path),'w') as file2:
             file2.write(temp)
 
 autoregex(input("Path to file: "))
